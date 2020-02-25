1b864d4b334a79044aa53b59be283e7e548d2c27
rijupramanik <pramanikriju@gmail.com>
2020-02-26 00:47:09 +0530

    Pet component and async API

--- src/Pet.js
+++ src/Pet.js
@@ -1,13 +1,22 @@
 import React from "react";
 
-const Pet = props => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
-  );
-};
+export default function Pet({ name, animal, breed, media, location, id }) {
+  let hero = "http://placevorgi.com/300/300";
+  if (media.length) {
+    hero = media[0].small;
+  }
 
-export default Pet;
+  return (
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>
+          {animal} - {breed} - {location}
+        </h2>
+      </div>
+    </a>
+  );
+}
