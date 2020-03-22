2e8ba3b6c47b5c040f28f850aa6b31efc32ed0cc
rijupramanik <pramanikriju@gmail.com>
2020-03-22 14:35:24 +0530

    Modal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Router, Link } from "@reach/router";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const themeHook = useState("peru");
   return (
-    <div>
-      <header>
-        <Link to="/">
-          <h1>Adopt Me!</h1>
-        </Link>
-      </header>
+    <ThemeContext.Provider value={themeHook}>
+      <div>
+        <header>
+          <Link to="/">
+            <h1>Adopt Me!</h1>
+          </Link>
+        </header>
 
-      <Router>
-        <SearchParams path="/" />
-        <Details path="details/:id" />
-      </Router>
-    </div>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="details/:id" />
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
